59bac7f11e3457285ae68877853e353eef1eedf1
Mabrey <32260556+Mabrey@users.noreply.github.com>
2019-01-20 21:08:38 -0800

    Finished mobile version of website.
    Implemented a sliding menu for navigation that is responsive.

--- Site/src/Components/Header.js
+++ Site/src/Components/Header.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react'
 import '../styles/Header.scss'
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            menuOpen: false,
+        }
+    }
     render(){
         return(
             <div id ='headerDiv'>
-                <div id='headerSideScrollMenu' >
-                    <span id = 'headerName'>Matthew Abrey</span>
-                </div>
-                <button id = 'hamburgerMenu'>{String.fromCharCode(9776)}</button>
+            
+                <span id = 'headerName' style = {this.state.menuOpen? {left: '-100%'} : {left: '5%'}} >Matthew Abrey</span>
+                <span id = 'menu' style = {this.state.menuOpen? {left: '3.5%'} : {left: '100%'}}>
+                    <a id='menuAbout' href="#aboutHeader">About</a>
+                    <a id='menuSkills' href="#skillsHeader">Skills</a>
+                    <a id='menuProjects' href="#projectHeader">Projects</a>
+                    <a id='menuContact' href="#contactHeader">Contact</a>
+                </span>
+                
+                <button id = 'hamburgerMenu' onClick = {() => {this.setState({...this.state, menuOpen: !this.state.menuOpen})}}>{String.fromCharCode(9776)}</button>
                 
             </div>
         );
@@ -18,9 +30,12 @@ export default Header
 
 
 
-// <div>
-//                     <a id='menuAbout' href="#aboutHeader">About</a>
-//                     <a id='menuSkills' href="#skillsHeader">Skills</a>
-//                     <a id='menuProjects' href="#projectHeader">Projects</a>
-//                     <a id='menuContact' href="#contactHeader">Contact</a>
-//                 </div>
+{/* <div>
+    <a id='menuAbout' href="#aboutHeader">About</a>
+    <a id='menuSkills' href="#skillsHeader">Skills</a>
+    <a id='menuProjects' href="#projectHeader">Projects</a>
+    <a id='menuContact' href="#contactHeader">Contact</a>
+</div> */}
+
+//
+//
